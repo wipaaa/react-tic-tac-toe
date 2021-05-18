63081bade32d77bc536f1902857fbe8def5f0ac6
wipaaa <ini.wipaaa@gmail.com>
2021-05-19 00:20:26 +0800

    Add calculate winner supprt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,11 +9,32 @@ const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [player, setPlayer] = useState(PLAYER_X);
 
+  const handleWinner = (sqrs) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const [x, y, z] of lines) {
+      if (sqrs[x] && sqrs[x] === sqrs[y] && sqrs[x] === sqrs[z]) {
+        return sqrs[x];
+      }
+    }
+
+    return !sqrs.filter((sqr) => !sqr).length ? 'Draw' : false;
+  };
+
   const handleOnClick = (position) => {
     const squares_ = squares.slice();
     const player_ = player === PLAYER_X ? PLAYER_O : PLAYER_X;
 
-    if (squares_[position]) {
+    if (handleWinner(squares_) || squares_[position]) {
       return;
     }
 
@@ -23,8 +44,12 @@ const App = () => {
     setSquares(squares_);
   };
 
+  const winner = handleWinner(squares);
+  const status = winner ? `Winner: ${winner}` : `Next: ${player}`;
+
   return (
     <>
+      <div>{status}</div>
       <Board {...{ squares, handleOnClick }} />
     </>
   );
